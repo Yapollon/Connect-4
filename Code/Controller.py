2147da1e07be58a864180b35435485d2e4c4dabd
Yapollon <75550334+Yapollon@users.noreply.github.com>
2022-06-11 00:14:11 +0200

    Quick reformating
    reformatted the code and deleted some unnecessary lines.

--- Code/Controller.py
+++ Code/Controller.py
@@ -22,7 +22,7 @@ class Controller:
             self.view.canvas.itemconfig(self.view.slots[view_slot], fill=View.grey[0])
             self.board.slots[board_slot].status = 0
         self.player = randint(1, 2)
-        self.color = self.change_color()
+        self.change_color()
         self.view.changePlayerText(f"Spieler {self.player} ist dran")
 
     def change(self, x):
@@ -30,6 +30,13 @@ class Controller:
         self.view.chipDropper((x, y), self.color)
         self.board.changeStatus((x, y), self.player)
         wonChips = self.board.checkIfWon((x, y), self.player)
+
+        # Deaktiviert den Button, wenn Reihe 1 erreicht wurde
+        LowestFreeSlots = [self.board.getLowestFree(column) for column in range(1, self.board.x_width + 1)]
+        if LowestFreeSlots[x - 1] is None:
+            self.view.deactivateButton(x)
+        elif LowestFreeSlots.count(None) == len(LowestFreeSlots):
+            self.view.gameEnd(), self.view.changePlayerText("Das Board ist voll!")
 
         if wonChips is None:
             self.player = self.change_player()
@@ -40,26 +47,16 @@ class Controller:
             self.view.changePlayerText(f"Spieler {self.player} hat gewonnen!")
             self.view.flash4([chip.position for chip in wonChips], self.color)
 
-        # Deaktiviert den Button, wenn Reihe 1 erreicht wurde
-        LowestFreeSlots = [self.board.getLowestFree(column) for column in range(1, self.board.x_width + 1)]
-        if LowestFreeSlots[x - 1] is None:
-            self.view.deactivateButton(x)
-        if LowestFreeSlots.count(None) == len(LowestFreeSlots):
-            self.view.gameEnd(), self.view.changePlayerText("Das Board ist voll!")
-
     def change_player(self):
-
         return 2 if self.player == 1 else 1
 
     def change_color(self):
-
         if self.player == 1:
             self.view.changeButtonColor(View.yellow)
             self.color = View.yellow
         elif self.player == 2:
             self.view.changeButtonColor(View.red)
             self.color = View.red
-        return self.color
 
 
 if __name__ == "__main__":
